60b8f2c90adc96e6857e1ab709dd7e789d9a80df
Mazumder <rudra.mazumder@capgemini.com>
2021-12-02 22:32:47 +0100

    first commit

---
+++ src/main/java/com/example/demo/UserConsumer.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import io.confluent.developer.User;
+
+@Service
+public class UserConsumer {
+	
+	private static final Logger log = LoggerFactory.getLogger(UserConsumer.class);
+
+
+	@KafkaListener(topics = "${topic.name}", groupId = "group_id")
+	public void consume(ConsumerRecord<String, User> record) {
+		log.info("Consumed message {}", record.value());
+	}
+
+}
